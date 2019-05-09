ae91d9fd3ab5dbc6b5e935ae710a3ef60f158bfb
Irene Villafane <irene.villafane@gmail.com>
2019-05-08 21:16:11 -0700

    added modal

--- client/src/pages/Books/Books.js
+++ client/src/pages/Books/Books.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/Grid";
 import { List, ListItem } from "../../components/List";
 import { Input, TextArea, FormBtn } from "../../components/Form";
-import { Navbar, NavItem } from 'react-materialize';
+import { Navbar, NavItem, Modal, Button} from 'react-materialize';
 // import Image from 'burger.jpg';
 
 // import img from './assets/images/burger.jpg';
@@ -68,22 +68,16 @@ class Books extends Component {
   render() {
     return (
       <div>
-        {/* <img src={img} /> */}
-        {/* <div style={ { backgroundImage: `url(require("./assets/images/burger.jpg"))` } }></div> */}
-        {/* <Modal header="Modal Header" trigger={<NavItem />}>
-        <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-        </p>
-        </Modal> */}
-         
-        
+  
         <Navbar brand={<a />} alignLinks="right">
-          <NavItem href="">
-            Login
-          </NavItem>
-          <NavItem href="components.html">
-            Create Profile
-          </NavItem>
+          <Modal trigger={<NavItem href="">Login</NavItem>}>
+
+          </Modal>
+
+          <Modal trigger={<NavItem href="">Create Profile</NavItem>}>
+
+          </Modal>
+
         </Navbar>
         <div className="background" style={{ backgroundImage: `url(${img})`, backgroundPosition: "center", backgroundRepeat: "no-repeat", height: "800px"}}>
 
