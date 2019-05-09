48803ca08a6bf019e7e317ff7e745f3346199702
Irene Villafane <irene.villafane@gmail.com>
2019-05-08 21:06:24 -0700

    background and nav added

--- client/srcTEMP/pages/Books/Books.js
+++ client/srcTEMP/pages/Books/Books.js
@@ -7,6 +7,15 @@ import { Col, Row, Container } from "../../components/Grid";
 import { List, ListItem } from "../../components/List";
 import { Input, TextArea, FormBtn } from "../../components/Form";
 import { Navbar, NavItem } from 'react-materialize';
+// import Image from 'burger.jpg';
+
+// import img from './assets/images/burger.jpg';
+let img = "./assets/images/burger.jpg"
+// const styles = {
+//   paperContainer: {
+//     backgroundImage: `url(${Image})`
+//   }
+// };
 
 
 
@@ -59,15 +68,15 @@ class Books extends Component {
   render() {
     return (
       <div>
-        {/* <Dropdown trigger={
-          <Button>Drop me!</Button>
-        }>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Bye</p>
-        </Dropdown> */}
-
+        {/* <img src={img} /> */}
+        {/* <div style={ { backgroundImage: `url(require("./assets/images/burger.jpg"))` } }></div> */}
+        {/* <Modal header="Modal Header" trigger={<NavItem />}>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+        </p>
+        </Modal> */}
+         
+        
         <Navbar brand={<a />} alignLinks="right">
           <NavItem href="">
             Login
@@ -76,6 +85,9 @@ class Books extends Component {
             Create Profile
           </NavItem>
         </Navbar>
+        <div className="background" style={{ backgroundImage: `url(${img})`, backgroundPosition: "center", backgroundRepeat: "no-repeat", height: "800px"}}>
+
+        </div>
 
       </div>
 
