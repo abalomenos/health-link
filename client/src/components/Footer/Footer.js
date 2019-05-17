7a85999116f773b8afdac6cdde22ead86dfc9efb
abalomenos <arisbalomenos@gmail.com>
2019-05-17 14:23:36 -0700

    Day and Footer update

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import {Footer, Row, Col, Container} from 'react-materialize';
+
+class Foot extends Component {
+  
+    render() {
+        return (
+            <div>
+                <Footer
+                    copyrights= {
+                        <div className="black-text">
+                            Health Link {'\u00A9'} 2019
+                        </div>
+                    }
+                    moreLinks={<a />}
+                    links={
+                        <ul>
+                            <p className="black-text center-align">
+                                Team
+                            </p>
+                            <li><a className="black-text text-lighten-1" href="#!">Argiris Balomenos</a></li>
+                            <li><a className="black-text text-lighten-1" href="#!">Charlie Glass</a></li>
+                            <li><a className="black-text text-lighten-1" href="#!">Anne Jackson</a></li>
+                            <li><a className="black-text text-lighten-1" href="#!">Irene Villafane</a></li>
+                        </ul>
+                    }
+                    className="example"
+                    >
+                    <Container>
+                        <Row>
+                            <Col  className="s12">
+                                <h5 className="black-text">
+                                    Health Link
+                                </h5>
+                                <p className="black-text">
+                                    Your one stop for Health and Fitness
+                                </p>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Footer>
+            </div>
+        );
+    }
+}
+  
+export default Foot;
+  
--- client/src/pages/Day/Day.js
+++ client/src/pages/Day/Day.js
@@ -1,66 +1,48 @@
 import React, { Component } from "react";
-import {Bar, Pie, Line} from 'react-chartjs-2';
-import {Row, Col} from 'react-materialize';
-import API from "../../utils/API";
+import {Bar, Pie} from 'react-chartjs-2';
+import {Container, Row, Col, Modal, TextInput, Button, Icon} from 'react-materialize';
+import DatePicker from "react-datepicker";
 
+import "react-datepicker/dist/react-datepicker.css";
+import './Day.css';
 
+// import API from "../../utils/API";
+
+let workoutImg = "./assets/images/workout1.jpg";
+const backgroundImg ='./assets/images/background1.jpg'
 
 class Day extends Component {
 
-  addOne = this.addOne.bind(this);
-  subOne = this.subOne.bind(this);
-  addOneWater = this.addOneWater.bind(this);
-  subOneWater = this.subOneWater.bind(this);
-
-  state = {
-    sleepCounter: 8,
-    waterCounter: 1,
-    proteinCounter: 90,
-    carbsCounter: 50,
-    fatCounter: 40,
-    caloriesCounter: 1500
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: new Date(),
+      sleepCounter: 8,
+      waterCounter: 1,
+      proteinCounter: 90,
+      carbsCounter: 50,
+      fatCounter: 40,
+      caloriesCounter: 1500,
+      maxCalories:  2000
+    };
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.addOneHour = this.addOneHour.bind(this);
+    this.subOneHour = this.subOneHour.bind(this);
+    this.addOneWater = this.addOneWater.bind(this);
+    this.subOneWater = this.subOneWater.bind(this);
+  }
 
   componentDidMount() {
 
   };
-// ****************** Test data ***************
 
-  nutrition = {
-    
+  handleDateChange(date) {
+    this.setState({
+      startDate: date
+    });
   }
 
-  calories = 1500;
-
-  workout = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets: [
-    {
-      label: 'My First dataset',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40]
-    }
-  ]
-  };
-  // ****************** Test data End ***************
-
-  addOne() {
+  addOneHour() {
     this.setState((prevState) => {
       return {
         sleepCounter : prevState.sleepCounter + 1
@@ -68,11 +50,10 @@ class Day extends Component {
      });
   }
 
-   subOne() {
-
+  subOneHour() {
     this.setState((prevState) => {
       return {
-        sleepCounter: prevState.sleepCounter == 0 ? prevState.sleepCounter: prevState.sleepCounter - 1
+        sleepCounter: prevState.sleepCounter === 0 ? prevState.sleepCounter: prevState.sleepCounter - 1
         };
      });
   }
@@ -87,118 +68,176 @@ class Day extends Component {
      });
   }
 
-   subOneWater() {
-
+  subOneWater() {
     this.setState((prevState) => {
       return {
-        waterCounter: prevState.waterCounter == 0 ? prevState.waterCounter: prevState.waterCounter - 1
+        waterCounter: prevState.waterCounter === 0 ? prevState.waterCounter: prevState.waterCounter - 1
         };
      });
   }
+  
 
   render() {
     return (
-      <div>
-        <Row>
-          <Col className="s2 offset-s7 black-text center-align">
-            Calories: {this.calories}
+      <div className="mainWrapper" style={{ backgroundImage: `url(${backgroundImg})` }}>
+        <Container className="containerDay">
+          <Row>
+          
+            <Col className="s4 offset-s1 black-text center-align">
+              <DatePicker
+                selected={this.state.startDate}
+                onChange={this.handleDateChange}
+              />
+            </Col>
+            <Col className="s4 offset-s2 black-text center-align">
+              Calories
+              <br/>
+              {this.state.caloriesCounter} / {this.state.maxCalories}
+            </Col>
+            
+          </Row>
+          <div className="divider"></div>
+          <Row>
+          <Col className="m4 offset-m1 s10 offset-s1 black-text center-align">
+            <div className="divider"></div>
+            <Bar
+              data={{
+                labels: ["Cups (1 cup = 8 oz.)"],
+                datasets: [{
+                  label: "Water Consumption",
+                  backgroundColor: 'rgb(0, 119, 190)',
+                  borderColor: 'rgb(0, 119, 190)',
+                  data: [this.state.waterCounter]
+                }]
+              }}
+              width={100}
+              height={100}
+              options={{
+                maintainAspectRatio: true,
+                legend: {
+                  labels: {
+                      fontColor: "black",
+                      fontSize: 16
+                  }
+                },
+                tooltips: {
+                  backgroundColor: "black",
+                  bodyFontColor: "white"
+                },
+                scales: {
+                  yAxes: [{
+                      ticks: {
+                          beginAtZero:true,
+                          min: 0,
+                          max: 8,
+                          fontColor: 'Black'
+                      }
+                    }]
+                }
+              }}
+            />
+            <br/>
+            <div className="btn red waves-effect" onClick={this.subOneWater}>-</div>
+            <div className="btn green waves-effect" onClick={this.addOneWater}>+</div>
           </Col>
-        </Row>
-        <Row>
-        <Col className="s2 offset-s3 white-text center-align">
-          <Bar
-            data={{
-              labels: ["Cups (1 cup = 8 fl. oz.)"],
-              datasets: [{
-                label: "Water Consumption",
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: [this.state.waterCounter]
-              }]
-            }}
-            width={100}
-            height={100}
-            options={{
-              maintainAspectRatio: true,
-              scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true,
-                        min: 0,
-                        max: 8   
-                    }
-                  }]
-               }
-            }}
-          />
-          <br/>
-          <input type='button' onClick={this.addOneWater} value='Inc'/>
-          <input type='button' onClick={this.subOneWater} value='Dec'/>
-        </Col>
-        <Col className="s2 offset-s2 white-text center-align" >
-          <Pie
-            width={100}
-            height={100}
-            data={{
-              labels: ["Calories"],
-              datasets: [{
-                data: [this.state.proteinCounter, this.state.carbsCounter, this.state.fatCounter],
-                backgroundColor : [
-                  "red", "green", "blue"
-                ]
-              }],
-              labels: [
-                  'Protein',
-                  'Carbs',
-                  'Fat'
-              ]
-            }}
-            options={{ maintainAspectRatio: true }}
-          />
-        </Col>
-        </Row>
-        <Row>
-        <Col className="s2 offset-s3 black-text center-align">
-          <label>
-            <input type="checkbox" class="filled-in" />
-            <span>Workout</span>
-          </label>
-        </Col>
-        <Col className="s2 offset-s2 black-text center-align">
-          <Bar
-            data={{
-              labels: ["Hours"],
-              datasets: [{
-                label: "Sleep Last Night",
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: [this.state.sleepCounter]
-              }]
-            }}
-            width={100}
-            height={100}
-            options={{
-              maintainAspectRatio: true,
-              scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true,
-                        min: 0,
-                        max: 10    
-                    }
-                  }]
-               }
-            }}
-          />
-          <br/>
-          <input type='button' onClick={this.addOne} value='Inc'/>
-          <input type='button' onClick={this.subOne} value='Dec'/>
-        </Col>
-        </Row>
-  
+          <Col className="m4 offset-m2 s10 offset-s1 black-text center-align">
+            <div className="divider"></div>
+            <Pie
+              width={100}
+              height={100}
+              data={{
+                labels: ["Protein", "Carbs", "Fat"],
+                datasets: [{
+                  data: [this.state.proteinCounter, this.state.carbsCounter, this.state.fatCounter],
+                  backgroundColor : [
+                    "rgb(246,148,33)", "rgb(169,168,173)", "rgb(238,30,37)"
+                  ]
+                }]
+              }}
+              options={{
+                maintainAspectRatio: true,
+                legend: {
+                  labels: {
+                      fontColor: "black",
+                      fontSize: 16
+                  }
+                },
+                tooltips: {
+                  backgroundColor: "black",
+                  bodyFontColor: "white"
+                }
+              }}
+            />
+            <br/>
+            <Modal trigger={<div className="btn green waves-effect">Add Meal</div>}>
+              <TextInput meal validate label="Meal" />
+              <Button type="submit" waves="light">Submit<Icon right>send</Icon></Button>
+            </Modal>
+          </Col>
+          </Row>
+          <Row>
+          <Col className="m4 offset-m1 s10 offset-s1 black-text center-align workoutContainer">
+            <div className="divider"></div>
+            
+            <div className="section">
+              Workout
+              <img src={workoutImg} alt="Workout" />
+            </div>
+            <div className="section">
+            <br/>
+            <label>
+              <input type="checkbox" className="filled-in" />
+              <span>Yes</span>
+            </label>
+            </div>
+          </Col>
+          <Col className="m4 offset-m2 s10 offset-s1 black-text center-align infoSection">
+            <div className="divider"></div>
+            <Bar
+              data={{
+                labels: ["Hours"],
+                datasets: [{
+                  label: "Sleep Last Night",
+                  backgroundColor: 'rgb(0, 200, 100)',
+                  borderColor: 'rgb(0, 200, 100)',
+                  data: [this.state.sleepCounter]
+                }]
+              }}
+              width={1}
+              height={1}
+              options={{
+                maintainAspectRatio: true,
+                legend: {
+                  labels: {
+                      fontColor: "black",
+                      fontSize: 16
+                  }
+                },
+                tooltips: {
+                  backgroundColor: "black",
+                  bodyFontColor: "white"
+                },
+                scales: {
+                  yAxes: [{
+                      ticks: {
+                          beginAtZero:true,
+                          min: 0,
+                          max: 10,
+                          fontColor: 'black'
+                      }
+                    }]
+                }
+              }}
+            />
+            <br/>
+            <div className="btn red waves-effect" onClick={this.subOneHour}>-</div>
+            <div className="btn green waves-effect" onClick={this.addOneHour}>+</div>
+          </Col>
+          </Row>
+          <div className="btn green waves-effect prev leftArrow">{'<'}</div>
+          <div className="btn green waves-effect next rightArrow">{'>'}</div>
+        </Container>
       </div>
-  
-  
   
     );
   }
