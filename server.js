8df59adc4becf8b04f8952f92972eedb63defd4e
abalomenos <arisbalomenos@gmail.com>
2019-05-21 21:42:54 -0700

    server.js update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const db = require('./models');
-
+const routes = require("./routes");
 const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3001;
